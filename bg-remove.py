dfaeb3b3b55e117312857f9df6c0afbe129c6684
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-01-28 19:57:38 +0530

    Add files via upload

---
+++ bg-remove.py
@@ -0,0 +1,91 @@
+import time, io, numpy as np
+from cv2 import MORPH_ELLIPSE, getStructuringElement 
+from PIL import Image
+from PIL.Image import Image as PILImage
+from typing import List, Union
+from enum import Enum
+from utils import new_session
+
+start_time = time.time()
+kernel = getStructuringElement(MORPH_ELLIPSE, (3, 3))
+
+def main():
+    # input_path = 'anya02.webp'
+    input_path = 'input.jpg'
+    output_path = 'output.png'
+
+    img = Image.open(input_path)
+    output = remove(img)
+    output.save(output_path)
+
+class ReturnType(Enum):
+    BYTES = 0
+    PILLOW = 1
+    NDARRAY = 2
+
+def naive_cutout(img: PILImage, mask: PILImage) -> PILImage:
+    empty = Image.new("RGBA", (img.size), 0)
+    cutout = Image.composite(img, empty, mask)
+    return cutout
+
+
+def get_concat_v_multi(imgs: List[PILImage]) -> PILImage:
+    pivot = imgs.pop(0)
+    for im in imgs:
+        pivot = get_concat_v(pivot, im)
+    return pivot
+
+
+def get_concat_v(img1: PILImage, img2: PILImage) -> PILImage:
+    dst = Image.new("RGBA", (img1.width, img1.height + img2.height))
+    dst.paste(img1, (0, 0))
+    dst.paste(img2, (0, img1.height))
+    return dst
+
+
+def remove(data: Union[bytes, PILImage, np.ndarray]
+           ) -> Union[bytes, PILImage, np.ndarray]:
+    if isinstance(data, PILImage):
+        return_type = ReturnType.PILLOW
+        img = data
+    elif isinstance(data, bytes):
+        return_type = ReturnType.BYTES
+        img = Image.open(io.BytesIO(data))
+    elif isinstance(data, np.ndarray):
+        return_type = ReturnType.NDARRAY
+        img = Image.fromarray(data)
+    else:
+        raise ValueError("Input type {} is not supported.".format(type(data)))
+
+    session = new_session("u2net")
+
+    masks = session.predict(img)
+    # print(masks)
+    cutouts = []
+
+    for mask in masks:
+        cutout = naive_cutout(img, mask)
+        cutouts.append(cutout)
+
+    cutout = img
+    if len(cutouts) > 0:
+        # print("len(cutouts) > 0 : true")
+        cutout = get_concat_v_multi(cutouts)
+
+    if ReturnType.PILLOW == return_type:
+        end_time = time.time()
+        execution_time = end_time - start_time
+        # print("Execution time: {:.3f} seconds".format(execution_time))
+        return cutout
+
+    if ReturnType.NDARRAY == return_type:
+        return np.asarray(cutout)
+
+    bio = io.BytesIO()
+    cutout.save(bio, "PNG")
+    bio.seek(0)
+
+    return bio.read()
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,76 @@
+import os, numpy as np
+from typing import Dict, List, Tuple
+import onnxruntime as ort
+from PIL import Image
+from PIL.Image import Image as PILImage
+
+class SimpleSession():
+    def __init__(self, model_name: str, inner_session: ort.InferenceSession):
+        self.model_name = model_name
+        self.inner_session = inner_session
+
+    def normalize(
+        self,
+        img: PILImage,
+        mean: Tuple[float, float, float],
+        std: Tuple[float, float, float],
+        size: Tuple[int, int],
+    ) -> Dict[str, np.ndarray]:
+        im = img.convert("RGB").resize(size, Image.LANCZOS)
+
+        im_ary = np.array(im)
+        im_ary = im_ary / np.max(im_ary)
+
+        tmpImg = np.zeros((im_ary.shape[0], im_ary.shape[1], 3))
+        tmpImg[:, :, 0] = (im_ary[:, :, 0] - mean[0]) / std[0]
+        tmpImg[:, :, 1] = (im_ary[:, :, 1] - mean[1]) / std[1]
+        tmpImg[:, :, 2] = (im_ary[:, :, 2] - mean[2]) / std[2]
+
+        tmpImg = tmpImg.transpose((2, 0, 1))
+
+        return {
+            self.inner_session.get_inputs()[0]
+            .name: np.expand_dims(tmpImg, 0)
+            .astype(np.float32)
+        }
+
+    def predict(self, img: PILImage) -> List[PILImage]:
+        ort_outs = self.inner_session.run(
+            None,
+            self.normalize(
+                img, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225), (320, 320)
+            ),
+        )
+        pred = ort_outs[0][:, 0, :, :]
+
+        ma = np.max(pred)
+        mi = np.min(pred)
+
+        pred = (pred - mi) / (ma - mi)
+        pred = np.squeeze(pred)
+
+        mask = Image.fromarray((pred * 255).astype("uint8"), mode="L")
+        mask = mask.resize(img.size, Image.LANCZOS)
+
+        return [mask]
+
+
+def new_session(model_name: str = "u2net"):
+    session_class = SimpleSession
+    
+    full_path = 'u2net.onnx'
+    sess_opts = ort.SessionOptions()
+
+    # if use more Threads for ONNX model during inference then
+    # make sess_opts.inter_op_num_threads = NoOfThread
+    if "OMP_NUM_THREADS" in os.environ:
+        sess_opts.inter_op_num_threads = int(os.environ["OMP_NUM_THREADS"])
+
+    return session_class(
+        model_name,
+        ort.InferenceSession(
+            str(full_path),
+            sess_options=sess_opts,
+            # providers=ort.get_available_providers(),
+        ),
+    )
