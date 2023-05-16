015072a519f0ea8d3d4c6f9e73c50943ceab91bc
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-05-16 07:24:36 +0530

    Update bg-remove.py

--- bg-remove.py
+++ bg-remove.py
@@ -63,7 +63,7 @@ def remove(data: Union[bytes, PILImage, np.ndarray]
     cutouts = []
 
     for mask in masks:
-        cutout = naive_cutout(img, mask, rev2)
+        cutout = naive_cutout(img, mask)
         cutouts.append(cutout)
 
     cutout = img
